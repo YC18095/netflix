540d007b2b56a4d76c8979f6c1596f5dd80a1d39
Richard Chan <tranchidung0318@gmail.com>
2022-10-22 11:31:20 +0700

    banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HomeScreen from './Components/HomeScreen';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./Components/HomeScreen";
 
 function App() {
   return (
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,48 +1,55 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import '../Components/Banner.css'
-import axios from '../ultil/axios'
-import requests from '../ultil/request';
+import React from "react";
+import { useState, useEffect } from "react";
+import "../Components/Banner.css";
+import axios from "../ultil/axios";
+import requests from "../ultil/request";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
 
-    const [movie, setMovies] = useState([])
-
-    const fetchData = async () => {
-        debugger
-        const res = await axios.get(requests.fetchNetflixOriginals)
-        if(res) {
-            setMovies(res.data.results[Math.floor(Math.random() * res.data.results.length - 1)])
-        }
+  const fetchData = async () => {
+    const res = await axios.get(requests.fetchNetflixOriginals);
+    if (res) {
+      setMovie(
+        res.data.results[
+          Math.floor(Math.random() * res.data.results.length - 1)
+        ]
+      );
+      return res;
     }
+  };
 
-    useEffect(()=> {
-        fetchData()
-    },[])
-     
-console.log(movie)
-    const truncated = (string, n) => {
-        return string?.length > n ? string.substr(0, n - 1) + '...' : string
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <header className="banner" style={{
-            backgroundImage: `url(https://media.istockphoto.com/photos/wall-black-background-for-design-stone-black-texture-background-picture-id1320815200?b=1&k=20&m=1320815200&s=612x612&w=0&h=yUGA6XiZ0zWU9lw5q3cm3ZHhiBd-Aqvx-HZClDjJo0o=)`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center'
-        }}>
-            <div className='banner__contents'>
-                <h1 className='banner__title'>Movie Name</h1>
-                <div className='banner__buttons'>
-                    <button className='banner__button'>Play</button>
-                    <button className='banner__button'>My List</button>
-                </div>
-                <h1 className='banner__description'>{truncated('This is Description This is Description This is Description This is Description This is DescriptionThis is DescriptionThis is DescriptionThis is DescriptionThis is DescriptionThis is DescriptionThis is DescriptionThis is DescriptionThis is Description', 100)}</h1>
-            </div>
+  const truncated = (string, n) => {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
 
-            <div className='banner__fadeBottom' />
-        </header>
-    );
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.name}</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncated(movie.overview, 150)}
+        </h1>
+      </div>
+
+      <div className="banner__fadeBottom" />
+    </header>
+  );
 }
 
 export default Banner;
