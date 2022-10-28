50a137b6309465139f79d56dad7dd21a011d913d
YC180995 <tranchidung0318@gmail.com>
2022-10-28 15:57:42 +0700

    profile screen css

--- src/Components/ProfileScreen.js
+++ src/Components/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
 import NavBar from '../Components/NavBar'
 import { selectUser } from "../features/userSlice";
-import {useSelector} from 'react-redux'
-import { useNavigate, useLocation } from "react-router-dom";
-import {auth} from '../fireBaseApp'
+import { useSelector } from 'react-redux'
+import { useLocation } from "react-router-dom";
+import '../Components/ProfileScreen.css'
+import { auth } from '../fireBaseApp'
 
 function ProfileScreen() {
   const { user } = useSelector(selectUser)
@@ -11,16 +12,18 @@ function ProfileScreen() {
   console.log('LOCATION', location)
   return (
     <div className="profileScreen">
-      <NavBar />
-      <h1>Edit Profile</h1>
       <div className="profile__body">
-        <img className="profile__logo" src="https://ionicframework.com/docs/img/demos/avatar.svg" alt="NetFlix Logo" />
-        <div className="profile__details">
-          <h2>{user.email}</h2>
-          <div className="profile__plans">
-            <button onClick={()=>auth.signOut()} className="profile__logout">
-              Sign Out
-            </button>
+        <NavBar />
+        <h1>Edit Profile</h1>
+        <div className="profile__info">
+          <img className="profile__logo" src="https://ionicframework.com/docs/img/demos/avatar.svg" alt="NetFlix Logo" />
+          <div className="profile__details">
+            <h2>{user.email}</h2>
+            <div className="profile__plans">
+              <button onClick={() => auth.signOut()} className="profile__logout">
+                Sign Out
+              </button>
+            </div>
           </div>
         </div>
       </div>
